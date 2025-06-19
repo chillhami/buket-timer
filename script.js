8e82e5bf06655a0226977c46b6ca3937bca0dc19
chillhami <onayilhami@hotmail.com>
2025-06-19 16:56:58 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+const startDate = new Date("2025-05-19T00:00:00");
+function updateTimer() {
+  const now = new Date();
+  const diff = now - startDate;
+  const seconds = Math.floor(diff / 1000) % 60;
+  const minutes = Math.floor(diff / (1000 * 60)) % 60;
+  const hours = Math.floor(diff / (1000 * 60 * 60)) % 24;
+  const days = Math.floor(diff / (1000 * 60 * 60 * 24));
+  document.getElementById("days").textContent = days;
+  document.getElementById("hours").textContent = hours;
+  document.getElementById("minutes").textContent = minutes;
+  document.getElementById("seconds").textContent = seconds;
+}
+setInterval(updateTimer, 1000);
+updateTimer();
